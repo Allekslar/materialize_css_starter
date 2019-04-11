28ad9b1e671c2101122ba716914f4c5c63aaa297
Allekslar <allekslara@gmail.com>
2019-04-11 12:33:39 +0300

    add browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,11 @@
 let mix = require('laravel-mix');
 
+// Path to dist folder
+const DIST = 'assets';
 
+// Proxy an existing virtual host (eg: 'myhost.dev', 'localhost/bootstrap-myhost').
+// If null, use the built-in static server.
+const PROXY = null;
 
 mix.options({
     clearConsole: true,
@@ -9,4 +14,14 @@ mix.options({
     .setPublicPath('/')
     .sass('resources/sass/theme.scss', 'assets/css')
     .js('resources/js/theme.js', 'assets/js')
-    .extract(['jquery', 'materialize-css']);
+    .extract(['jquery', 'materialize-css'])
+    .browserSync({
+        server: PROXY === null,
+        proxy: PROXY,
+        files: [
+            '*.{php|html|htm|js}',
+            DIST + '/**/*',
+            // '*.*'
+
+        ]
+    });
